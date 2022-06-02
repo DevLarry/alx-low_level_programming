ad2e3e2791c71afc1c3abeda4ddba9e30b6f40ed
DevLarry <olanrewajusamad070@gmail.com>
2022-06-02 18:43:50 -0500

    The put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a quote
+ * Description: It prints "Programming is like building a..."
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
