05bb48758db132c6cf85db64ed1225383cd81df2
DevLarry <olanrewajusamad070@gmail.com>
2022-06-03 17:50:48 -0500

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: this is where all code is
+* Return: 0
+*/
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0; alpha <= 9; alpha++)
+		printf("%d", alpha);
+	printf("\n");
+	return (0);
+}
