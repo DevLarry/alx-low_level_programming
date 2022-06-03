263f29a816a8e5d09cd92cb76e22546419a75e40
DevLarry <olanrewajusamad070@gmail.com>
2022-06-02 19:01:35 -0500

    The printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a quote
+ * Description: It prints "Programming is like building a..."
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
