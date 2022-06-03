b0f3bd9ed9abf5991f2d8f8d6f7e56fc7d1721c0
DevLarry <olanrewajusamad070@gmail.com>
2022-06-03 17:11:43 -0500

    Print all the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: this is where all code is
+* Return: 0
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
