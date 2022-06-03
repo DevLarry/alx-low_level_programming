015887502ba9f25e00b2ddb1c5102e8f922f85a6
DevLarry <olanrewajusamad070@gmail.com>
2022-06-02 19:40:42 -0500

    Use the error stream

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a quote
+ * Description: It prints "Programming is like building a..."
+ * Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
