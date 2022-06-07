1d073acc51a43baecf1e84ea3905b5a5f60e0ba9
DevLarry <olanrewajusamad070@gmail.com>
2022-06-07 15:24:59 -0500

    Print Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+* Description: this is where all code is
+* Return: 0
+*/
+int main(void)
+{
+	char word[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
