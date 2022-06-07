0754a4277dc188da715e693be90c2033e1e3d0db
DevLarry <olanrewajusamad070@gmail.com>
2022-06-07 18:02:40 -0500

    Print absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,13 @@
  * _abs  - checks if a character is an alphabet
  * @n: takes a character
  * Description: Checks whether the character(c) is an alphabet
- * Return: 1 if c is an alphabet an returns 0 otherwise
+ * Return: if c is an alphabet an returns 0 otherwise
  */
-int _abs(char n)
+int _abs(int n)
 {
-	if (n < 0)
+	if (n >= 0)
 	{
-		return (n*-1);
+		return (n);
 	}
-	return n;
-	return (0);
+	return (n * -1);
 }
