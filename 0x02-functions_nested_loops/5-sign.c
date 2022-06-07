80083a17e5eab6eafb6263abd75844d727d9a8ae
DevLarry <olanrewajusamad070@gmail.com>
2022-06-07 17:55:41 -0500

    Print absolute

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,26 @@
+#include "main.h"
 /**
- * _isalpha  - checks if a character is an alphabet
- * @c: takes a character
+ * print_sign  - checks if a character is an alphabet
+ * @n: takes a character
  * Description: Checks whether the character(c) is an alphabet
  * Return: 1 if c is an alphabet an returns 0 otherwise
  */
-int _isalpha(char c)
+int print_sign(char c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (n > 1)
 	{
+		_putchar('+');
 		return (1);
+	}
+	else if (n < 1)
+	{
+		_putchar('-');
+		return (-1)
+	}
+	else
+	{
+		_putchar('0')
+		return (0);
 	}
 
 	return (0);
