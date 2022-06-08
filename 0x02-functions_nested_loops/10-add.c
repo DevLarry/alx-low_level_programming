af330618c9fce89c741656b5f6c1d386135192e1
DevLarry <olanrewajusamad070@gmail.com>
2022-06-08 22:05:07 +0100

    Times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add  - Prints the last digit of an integer
+ * @a: first number
+ * @b: second number
+ * Description: Prints the last digit of numbers
+ * Return: returns the sum of the two numbers (int)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
