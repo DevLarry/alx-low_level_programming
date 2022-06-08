74b8e4089f4921320479e51219408d84e7a3494f
DevLarry <olanrewajusamad070@gmail.com>
2022-06-08 12:30:02 -0500

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,37 +1,23 @@
 #include "main.h"
 /**
  * jack_bauer  - Prints the last digit of an integer
- * @n: the number to be evaluated
  * Description: Prints the last digit of numbers
  * Return: returns the last digit (int)
  */
 void jack_bauer(void)
 {
 	int i, j;
-	
-	for (i = 0; i < 23; i++)
+
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 59; j++)
+		for (j = 0; j < 60; j++)
 		{
-			if(i < 10)
-			{
-				_putchar('0');
-				_putchar(i + '0');
-			}
-			else
-			{
-				_putchar(i + '0');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			if(j < 10)
-			{
-				_putchar('0');
-				_putchar(j + '0');
-			}
-			else
-			{
-				_putchar(j + '0');
-			}
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
