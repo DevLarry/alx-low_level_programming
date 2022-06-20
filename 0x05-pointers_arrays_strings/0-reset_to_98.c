afe43ddafe634809e9b8324d3c653459151e355b
DevLarry <olanrewajusamad070@gmail.com>
2022-06-20 17:03:36 -0500

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+* reset_to_98 - Reset the value of a pointed address to 98
+* @n: pointer to the value to be reset
+* description: It takes a pointer and set the value pointed to 98
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
