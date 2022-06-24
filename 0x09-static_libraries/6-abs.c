619c501b0589252ef599546a022f5dbe3592a652
DevLarry <olanrewajusamad070@gmail.com>
2022-06-24 22:51:01 +0100

    Today's task

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,17 +1,12 @@
 #include "main.h"
 /**
- * _abs - compute the absolute value of an integer
- * @n: int type number
- * Return: absolute value of @n
+ * _abs - function that prints the absolute value of a number
+ * @n: takes in an integer
+ * Return: 1 if > 0, 0 if == 0, positive if negative
  */
 int _abs(int n)
 {
 	if (n < 0)
-	{
 		return (n * -1);
-	}
-	else
-	{
-		return (n);
-	}
+	return (n);
 }
